3b68235adb28e8c7b26bdd92de7b1c73f4cccaf6
Matthew Frost <m.frost@mattronix.nl>
2016-08-02 22:13:56 +0200

    added manifest file

--- scripts/bootstrap-server.sh
+++ scripts/bootstrap-server.sh
@@ -1,4 +1,15 @@
-in-target mkdir --mode=700 -p /target/root/.ssh
-in-target wget -O /root/.ssh/authorized_keys https://raw.githubusercontent.com/sha2017/preseed-profiles-debian/master/files/sshkeys.pub
-in-target chmod -R 700 /root/.ssh/
-exit
+#!/bin/bash
+
+echo "Post install started on `date`" > /root/manifest
+
+echo "* Installing SSH Keys..." >> /root/manifest
+mkdir --mode=700 -p /target/root/.ssh
+wget -O /root/.ssh/authorized_keys https://raw.githubusercontent.com/sha2017/preseed-profiles-debian/master/files/sshkeys.pub
+chmod -R 700 /root/.ssh/
+
+echo "Post install completed on `date`" >> /root/manifest
+
+
+
+
+exit 0
