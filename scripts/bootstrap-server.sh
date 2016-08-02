a09afce12110b31eea57a9763452ff0f958f2cfc
Matthew Frost <m.frost@mattronix.nl>
2016-08-02 20:42:01 +0200

    Update bootstrap-server.sh

--- scripts/bootstrap-server.sh
+++ scripts/bootstrap-server.sh
@@ -1,4 +1,6 @@
+in-target echo "Starting Bootstrap process." >> /root/bootstrap.log
 in-target mkdir --mode=700 -p /target/root/.ssh
+in-target echo " .ssh Directory Created." >> /root/bootstrap.log
 in-target wget -O /root/.ssh/authorized_keys https://raw.githubusercontent.com/sha2017/preseed-profiles-debian/master/files/sshkeys.pub
-
+in-target echo "Public SSH keys downloaded." >> /root/bootstrap.log
 
