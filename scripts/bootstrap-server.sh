974db6ad5e8d7dca8badc8ef7776d69feca34848
Matthew Frost <m.frost@mattronix.nl>
2016-08-02 20:31:08 +0200

    Update bootstrap-server.sh

--- scripts/bootstrap-server.sh
+++ scripts/bootstrap-server.sh
@@ -1,3 +1,3 @@
 in-target mkdir -p /target/root/.ssh
 in-target wget -O /root/.ssh/authorized_keys https://raw.githubusercontent.com/sha2017/preseed-profiles-debian/master/files/sshkeys.pub
-in-target chmod -R 700 /target/root/.ssh
+
