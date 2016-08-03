f829f514fbbcf3a02307d8638a781b6df50c8b12
Matthew Frost <m.frost@mattronix.nl>
2016-08-03 13:11:14 +0200

    Update bootstrap-server.sh

--- scripts/bootstrap-server.sh
+++ scripts/bootstrap-server.sh
@@ -3,7 +3,7 @@
 echo "Post install started on `date`" > /root/manifest
 
 echo "* Installing SSH Keys..." >> /root/manifest
-mkdir --mode=700 -p /target/root/.ssh
+mkdir --mode=700 -p /root/.ssh
 wget -O /root/.ssh/authorized_keys https://raw.githubusercontent.com/sha2017/preseed-profiles-debian/master/files/sshkeys.pub
 chmod -R 700 /root/.ssh/
 
